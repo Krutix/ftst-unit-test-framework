a0f74eea5184fd22ad27ea5b8d53f0c5ed0543e0
Krutix <krutix@localhost.localdomain>
2021-01-10 01:06:04 +0300

    [run_test] simplify run test and move timer in run_test body

--- examples/test_example.c
+++ examples/test_example.c
@@ -1,31 +1,31 @@
 #include <stdint.h>
 #include <fcntl.h>
-#include "ft_speed_test.h"
+#include "../include/ft_speed_test.h"
 
 #define TEST_PRINTF(F, ...) printf(F, __VA_ARGS__)
 
 #define NAME(name) #name
 #define RESULT_FILE NAME(std) "_result"
 
-FTST_TEST(simple_integer,
+FTST_TEST(simple_integer)
 {
     for (int i = 0; i < 50000; i++)
         TEST_PRINTF("%i", i);
-})
+}
 
-FTST_TEST(complex_format_int,
+FTST_TEST(complex_format_int)
 {
     for (long long i = INT64_MIN; i < INT64_MIN + 20000l; i++)
         TEST_PRINTF("%050lli", i);
     for (long long i = INT64_MAX; i > INT64_MAX - 20000l; i--)
         TEST_PRINTF("%+20lli", i);
-})
+}
 
 int main()
 {
     //ftst_init(RESULT_FILE);
     ftst_init(NULL); // result go to stderr
 
-    ftst_run_test(FTST_TEST_CASE(simple_integer));
-    ftst_run_test(FTST_TEST_CASE(complex_format_int));
+    FTST_RUNTEST(simple_integer);
+    FTST_RUNTEST(complex_format_int);
 }
--- include/ft_speed_test.h
+++ include/ft_speed_test.h
@@ -5,32 +5,27 @@
 # include <stdlib.h>
 # include <stdio.h>
 
-FILE        *g_ftst_fptr;
+FILE*       g_ftst_fptr;
 clock_t     g_ftst_start;
-typedef void(*ftst_test_t)(void);
+typedef     void(*ftst_test_t)(void);
 
 # define FTST_TEST_CASE(test_name) &ftst_test_case_##test_name
+# define FTST_TEST_CASE_NAME(test_name) #test_name
 
-# define FTST_TEST(test_name, test_body)  \
-void ftst_test_case_##test_name(void)     \
-{                                         \
-    ftst_start_timer();                   \
-    test_body                             \
-    ftst_end_timer(#test_name);           \
-}
+# define FTST_TEST(test_name)                    \
+void ftst_test_case_##test_name(void)
 
 # define FTST_ERROR(error_message) ftst_error(#error_message)
-void ftst_error(char const *error_message)
+void ftst_error(char const* error_message)
 {
     fprintf(stderr, "ftst error | %s\n", error_message);
     exit(-1);
 }
 
 void ftst_start_timer() { g_ftst_start = clock(); }
-void ftst_end_timer(const char *timer_name)
-    { if (g_ftst_fptr) fprintf(g_ftst_fptr, "%s,%fms\n", timer_name, (float)(clock() - g_ftst_start) / 1000.); }
+clock_t ftst_end_timer() { return clock() - g_ftst_start; }
 
-void ftst_init(char const *file_name)
+void ftst_init(char const* file_name)
 {
     if (file_name != NULL)
     {
@@ -50,9 +45,15 @@ void ftst_exit(void)
     g_ftst_fptr = NULL;
 }
 
-void ftst_run_test(ftst_test_t test_case)
+#define FTST_RUNTEST(test_name) ftst_run_test(FTST_TEST_CASE(test_name), FTST_TEST_CASE_NAME(test_name))
+void ftst_run_test(ftst_test_t test_case, char const* test_case_name)
 {
+    clock_t test_time;
+
+    ftst_start_timer();
     test_case();
+    test_time = ftst_end_timer();
+    fprintf(g_ftst_fptr, "%s,%fms\n", test_case_name, test_time / 1000.);
 }
 
 #endif
