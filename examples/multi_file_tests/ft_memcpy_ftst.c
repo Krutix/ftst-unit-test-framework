69654770326854ad546b86b6ac8ce39fecc24466
Krutix <krutix@localhost.localdomain>
2021-01-26 13:05:53 +0300

    [example] add two little test to memcpy

--- examples/multi_file_tests/ft_memcpy_ftst.c
+++ examples/multi_file_tests/ft_memcpy_ftst.c
@@ -8,5 +8,7 @@ TEST(ft_memcpy_basic_test)
 {
     char buff[256];
     STR_EQ(ft_memcpy(buff, "aaaaaaa", 8), "aaaaaaa");
+    STR_EQ(buff, "aaaaaaa");
     STR_EQ(ft_memcpy(buff, "bbbb", 4), "bbbbaaa");
+    STR_EQ(buff, "bbbbaaa");
 }
