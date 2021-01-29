be7b204cdea3655c5fca0e47efda99f09ded30f7
Krutix <krutix@localhost.localdomain>
2021-01-30 00:02:54 +0300

    [generator] 0 files handle

--- gen_test_main.py
+++ gen_test_main.py
@@ -7,26 +7,35 @@ def get_all_tests(files : [str]) -> [str]: # get list of test files and fetch al
     cmd = f'grep {pattern} -h {" ".join(str(file) for file in files)}'
     output = subprocess.check_output(cmd, shell=True)
     return str(output).strip("'b\\n").split("\\n")
-    
 
-ftst_test_runner = open('ftst_test_runner.c', 'w')
+def create_test_run_main(files):
+    ftst_test_runner = open('ftst_test_runner.c', 'w')
 
-ftst_test_runner.write("#include \"ftst.h\"\n")
-ftst_test_runner.write("\n")
+    ftst_test_runner.write("#include \"ftst.h\"\n")
+    ftst_test_runner.write("\n")
 
-tests = get_all_tests(sys.argv[1::])
-for test in tests:
-    ftst_test_runner.write(f"{test};\n")
+    # test functions declaration
+    tests = get_all_tests(files)
+    for test in tests:
+        ftst_test_runner.write(f"{test};\n")
 
-ftst_test_runner.write("\n")
-ftst_test_runner.write("int main()\n")
-ftst_test_runner.write("{\n")
-ftst_test_runner.write("\tFTST_INIT();\n")
-ftst_test_runner.write("\n")
+    ftst_test_runner.write("\n")
+    # main function
+    ftst_test_runner.write("int main()\n")
+    ftst_test_runner.write("{\n")
+    ftst_test_runner.write("\tFTST_INIT();\n")
+    ftst_test_runner.write("\n")
 
-for test in tests:
-    ftst_test_runner.write(f"\t{test.replace('TEST', 'RUNTEST')};\n")
+    # run test functions
+    for test in tests:
+        ftst_test_runner.write(f"\t{test.replace('TEST', 'RUNTEST')};\n")
 
-ftst_test_runner.write("\n")
-ftst_test_runner.write("\tFTST_EXIT();\n")
-ftst_test_runner.write("}\n")
+    ftst_test_runner.write("\n")
+    ftst_test_runner.write("\tFTST_EXIT();\n")
+    ftst_test_runner.write("}\n")
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        exit
+    create_test_run_main(sys.argv[1:])
